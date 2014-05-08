3fbcda2b723c2392687f71a1d4b0def5c34495a1
Naimdjon Takhirov <naimdjon@gmail.com>
2014-05-08 20:10:19 +0200

    problem1 with solution

--- src/main/java/project_euler/Problem1.java
+++ src/main/java/project_euler/Problem1.java
@@ -4,11 +4,10 @@ public class Problem1 {
 
     public static void main(String[] args) {
         int result=run();
-        System.out.println("Sum av multiple av 3 og 5 er: " + result);
+        System.out.println("Sum of multiples of 3 and 5 is: " + result);
     }
 
     public static int run() {
-
         int sum = 0;
         for (int i = 3; i < 1000; i++) {
             if (i % 3 == 0 || i % 5 == 0) {
