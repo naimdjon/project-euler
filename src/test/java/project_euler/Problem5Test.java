c0dcb8f0ac4e8bc66b4162d347cdf930dcd9879f
Naimdjon <naimdjon@gmail.com>
2014-06-06 23:23:39 +0200

    working on problem 5

---
+++ src/test/java/project_euler/Problem5Test.java
@@ -0,0 +1,15 @@
+package project_euler;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Problem5Test {
+
+    @Test
+    public void testRunProblem5() {
+        assertEquals(123,Problem5.run());
+    }
+
+
+}
